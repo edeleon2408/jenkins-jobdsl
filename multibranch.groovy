f423ea98d8bc6135863dab4849a4040409a93205
Erwin De León <Erwin.De.Leon@ibm.com>
2020-04-02 21:42:38 -0500

    Creando archivo multi branch JOBDSL

---
+++ multibranch.groovy
@@ -0,0 +1,15 @@
+multibranchPipelineJob('multi-branch-docker-war') {
+	triggers{
+		periodic(1)
+	}
+    branchSources {
+        git {
+            remote('https://github.com/edeleon2408/jenkins-docker-war.git')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+    }
+}
