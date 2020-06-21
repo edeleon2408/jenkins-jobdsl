7089a75f92b6ee2d11511e57e5e392410dfa75bb
Erwin de León Martínez <erwin.deleon@ASD27868.local>
2020-06-20 20:21:54 -0500

    subiendo

--- jenkinsfolder.groovy
+++ jenkinsfolder.groovy
@@ -2,3 +2,21 @@ folder('Enrolamiento'){
 displayName('Enrolamiento')
 description('Folder para proyectos de Enrolamientos')
 }
+
+multibranchPipelineJob('Enrolamiento/mb-project-a-angular-docker') {
+triggers {
+        periodic(1)
+    }
+    branchSources {
+        git {
+id('angular-docker-id')
+            remote("https://github.com/edeleon2408/angular-docker.git")
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+    }
+}
+
